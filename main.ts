9c6127a13ff40ff895ad80ddb9432bc14549fb29
default <miles.aube@stu.ocsb.ca>
2025-12-01 21:49:10 +0000

    Program for happy birthday

---
+++ main.ts
@@ -0,0 +1,17 @@
+/**
+ * @author Miles Aube
+ * @version 1.0.0
+ * @date 2025-12-01
+ * @fileoverview This program that asks user for first name then outputs happy birthday song
+ */
+//variable
+let UserName: String;
+
+// input
+UserName = prompt("What is your name?") || "No name entered!";
+
+//output 
+console.log("Happy birthday to you");
+console.log("Happy birthday to you");
+console.log("Happy birthday dear " + UserName);
+console.log("Happy birthday to you!");
